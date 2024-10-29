9eaeee062aac54c46a0b383833c1513ddf405c2b
Hifza120 <l1s23bscs0012@ucp.edu.pk>
2024-10-29 10:50:36 +0500

    Add files via upload

--- myList.h
+++ myList.h
@@ -1,8 +1,8 @@
 #include"List.h"
 template<typename T>
-class myList:public List<T>{
+class myList :public List<T>{
 public:
-	myList(int mSize=5) :List<T>(mSize){}
+	myList(int mSize = 5) :List<T>(mSize){}
 	myList(const myList & L2){
 		List<T>::arr = new T[L2.List<T>::maxSize];
 		maxSize = L2.maxSize;
@@ -11,41 +11,42 @@ public:
 			List<T>::arr = L2.List<T>::arr[i];
 		}
 	}
+ void removeElementFromStart();
 	void addElementatFirstIndex(T value);
-	 void addElementatLastIndex(T value);
-	  T removeElementFromEnd();
-	  bool empty(){
-		  return  List<T>::currentSize == 0;
-	  }
-	  bool Full(){
-		  return List<T>::maxSize == List<T>::currentSize;
-	  }
-	  int Size(){
-		  return List<T>::maxSize;
-	  }
-	  bool insertAt(T value, int index){
-		  if (currentSize - 1 > index){
-			  List<T>::arr[index] = value;
-			  currentSize++;
-			  return true;
-		  }
-		  return false;
-	  }
-	  T last(){
-		  return List<T>::arr[--currentSize];
-	  }
-	  bool Search(T value){
-		  for (int i = 0; i < List<T>::currentSize; i++){
-			  if (value == List<T>::arr[i]){
-				  return true;
-			  }
-		  }
-		  return false;
-	  }
+	void addElementatLastIndex(T value);
+	T removeElementFromEnd();
+	bool empty(){
+		return  List<T>::currentSize == 0;
+	}
+	bool Full(){
+		return List<T>::maxSize == List<T>::currentSize;
+	}
+	int Size(){
+		return List<T>::maxSize;
+	}
+	bool insertAt(T value, int index){
+		if (currentSize - 1 > index){
+			List<T>::arr[index] = value;
+			currentSize++;
+			return true;
+		}
+		return false;
+	}
+	T last(){
+		return List<T>::arr[--currentSize];
+	}
+	bool Search(T value){
+		for (int i = 0; i < List<T>::currentSize; i++){
+			if (value == List<T>::arr[i]){
+				return true;
+			}
+		}
+		return false;
+	}
 
-	  virtual  ~myList(){
+	  ~myList(){
 
-	  }
+	}
 };
 template<typename T>
 void myList<T>::addElementatFirstIndex(T value){
@@ -66,15 +67,15 @@ void myList<T>::addElementatLastIndex(T value){
 
 }
 template<typename T>
-T  myList<T>:: removeElementFromEnd(){
+T  myList<T>::removeElementFromEnd(){
 	return List<T>::arr[--List<T>::currentSize];
 }
 template<typename T>
-void removeElementFromStart(){
-		for (int i = 0 i < List<T>::currentSize; i++){
-			List<T>::arr[i] = List<T>::arr[i + 1];
-		}
+void myList<T>::removeElementFromStart(){
+	for (int i = 0; i < List<T>::currentSize; i++){
+		List<T>::arr[i] = List<T>::arr[i + 1];
+	}
 
-		List<T>::currentSize--;
+	List<T>::currentSize--;
 
 }
